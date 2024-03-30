08d97fdebe0dfc819e0a8def0240b9a3ad8453a0
Michael McCourt <mmccourt928@outlook.com>
2024-03-30 19:19:35 +0000

    user bjj move variables added

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+const firstMove = ['Attempt Takedown', 'Pull guard'];
+const moveTop = ['Pass Guard', 'Attempt Leglock'];
+const moveBottom = ['Attempt Sweep', 'Attempt Submission'];
+const block = 'Blocked move';
+const guardPassed = ['Attempt side control', 'Attempt for mount']
+
+
