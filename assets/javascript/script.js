ffc14ab644f4580e98fb75af153c7434bc90b5be
Michael McCourt <mmccourt928@outlook.com>
2024-03-30 19:53:49 +0000

    dom content loaded inner html set for game section

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,22 @@
+document.addEventListener('DOMContentLoaded', gameOpener)
 const firstMove = ['Attempt Takedown', 'Pull guard'];
 const moveTop = ['Pass Guard', 'Attempt Leglock'];
 const moveBottom = ['Attempt Sweep', 'Attempt Submission'];
 const block = 'Blocked move';
 const guardPassed = ['Attempt side control', 'Attempt for mount']
+
+const gameStart = document.querySelector('#main-game').innerHtml(``)
+
+function gameOpener(){
+    document.querySelector('#main-game').innerHTML = `<div class="start-menu">
+    <p>Start Match</p>
+</div>
+<div class="start-menu">
+    <p>Choose your belt</p>
+</div>
+<div class="start-menu">
+    <p>Tutorial</p>
+</div>`
+}
 
 
